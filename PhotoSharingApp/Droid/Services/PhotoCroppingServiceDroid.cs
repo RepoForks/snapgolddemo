7ad4c286c122b0b1f093a2a7fe442e33a2b2680b
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-09-15 12:02:34 +0200

    Add CroppingService to Android

--- PhotoSharingApp/Droid/Services/PhotoCroppingServiceDroid.cs
+++ PhotoSharingApp/Droid/Services/PhotoCroppingServiceDroid.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Android.Graphics;
 using PhotoSharingApp.Forms.Droid.Services;
 using PhotoSharingApp.Frontend.Portable.Abstractions;
 
@@ -9,7 +11,26 @@ namespace PhotoSharingApp.Forms.Droid.Services
     {
         public byte[] ResizeImage(byte[] source, int width, int height)
         {
-            return null;
+            using (var originalImage = BitmapFactory.DecodeByteArray(source, 0, source.Length))
+            {
+                // Determine the ratio
+                var ratioX = (double)width / originalImage.Width;
+                var ratioY = (double)height / originalImage.Height;
+                var ratio = Math.Min(ratioX, ratioY);
+
+                // Calculate the new width/height
+                var newWidth = (int)(originalImage.Width * ratio);
+                var newHeight = (int)(originalImage.Height * ratio);
+
+                using (var resizedImage = Bitmap.CreateScaledBitmap(originalImage, newWidth, newHeight, false))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        resizedImage.Compress(Bitmap.CompressFormat.Jpeg, 100, ms);
+                        return ms.ToArray();
+                    }
+                }
+            }
         }
     }
 }
